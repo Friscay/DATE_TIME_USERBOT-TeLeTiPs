87ee96c83c6d0b0011980c8bda7bc7e0ffb42cd0
Zenzuzu <97756244+ZenxMato@users.noreply.github.com>
2023-03-26 02:44:09 +0700

    Update quotes_teletips.py

--- lists_teletips/quotes_teletips.py
+++ lists_teletips/quotes_teletips.py
@@ -15,4 +15,17 @@ quotes_teletips = [
 "Trusted & Aman, t.me/zenzustore",
 "Keep Silent | t.me/zenzustore",
 "Ini Bio Otomatis, Silakan ke t.me/zenzustore"
+"Life is a question and how we live it is our answer.", 
+"It’s better to be a lion for a day than a sheep all your life.", 
+"You must be the change you wish to see in the world.", 
+"Our life is frittered away by detail. Simplify, simplify.", 
+"Good friends, good books, and a sleepy conscience: this is the ideal life", 
+"The journey of a thousand miles begins with one step.", 
+"The mind is everything. What you think you become.", 
+"Go confidently in the direction of your dreams! Live the life you’ve imagined.", 
+"Don’t cry because it’s over, smile because it happened.", 
+"Get busy living or get busy dying.", 
+"To live is the rarest thing in the world. Most people exist, that is all.", 
+"Not how long, but how well you have lived is the main thing.", 
+"Life isn’t a matter of milestones, but of moments"
 ]
