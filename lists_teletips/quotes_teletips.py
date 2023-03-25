f3d5c2fb53f2ac3b178789752f2775e1fc544595
Zenzuzu <97756244+ZenxMato@users.noreply.github.com>
2023-03-26 02:26:06 +0700

    Update quotes_teletips.py

--- lists_teletips/quotes_teletips.py
+++ lists_teletips/quotes_teletips.py
@@ -2,17 +2,17 @@
 
 quotes_teletips = [
 "Mau Order? Cek t.me/zenzustore", 
-"Dor, | t.me/zenzustore",
+"Aplikasi Premium | t.me/zenzustore",
 "Still open, t.me/zenzustore",
 "Silent is Gold | t.me/zenzustore",
-"Ytta | t.me/zenzustore",
+"t.me/videolucutelegram | t.me/zenzustore",
 "Order? Chat aja.",
-"Puasa Lancar? Order Yuk.",
-"Aplikasi Premium, Sosmed? Ada semua.",
-"Open Terus Pokoknya.",
+"Puasa Lancar? Order Yuk t.me/zenzustore",
+"Aplikasi Premium, Sosmed? Ada semua di t.me/zenzustore",
+"Open Terus Pokoknya, t.me/zenzustore",
 "Gausah ragu cek t.me/zenzustore aja",
-"Yuk cek t.me/zenzustore ya.",
-"Ini Bio Otomatis.",
-"𝗦𝗶𝗹𝗲𝗻𝘁 𝗶𝘀 𝗚𝗼𝗹𝗱 |t.me/zenzustore",
-"Jangan Lupa Puasa Ya."
+"Follow t.me/zenzustore",
+"Trusted & Aman, t.me/zenzustore",
+"Keep Silent | t.me/zenzustore",
+"Ini Bio Otomatis, Silakan ke t.me/zenzustore"
 ]
