d38bf279ba5a3f593dd8a9962ae23a5b9fb910fe
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2023-06-22 06:12:15 +0700

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -6,9 +6,6 @@
 
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
-from lists_teletips.quotes_teletips import *
-from lists_teletips.emojis_teletips import *
-from PIL import Image, ImageDraw, ImageFont
 import datetime
 import pytz
 import asyncio
@@ -27,19 +24,11 @@ async def main_teletips():
     try:
         while True:
             if Date_Time_Userbot_teletips.is_connected:
-                Quotes_teletips = random.choice(quotes_teletips)
-                Emojis_teletips = random.choice(emojis_teletips)
+                pp = random.choice(randompp)
                 TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
                 Time_teletips = TimeZone_teletips.strftime("%I:%M %p")
-                Date_teletips = TimeZone_teletips.strftime("%b %d") 
-                Image_teletips = Image.open("image.jpg")
-                Image_font_teletips = ImageFont.truetype("ds-digit.ttf", 360)
-                Image_text_teletips = f"{Time_teletips}"
-                Image_edit_teletips = ImageDraw.Draw(Image_teletips)
-                Image_edit_teletips.text((690, 550), Image_text_teletips, (0, 255, 255), font = Image_font_teletips)
-                Image_teletips.save("Image_final_teletips.jpg")
-                await Date_Time_Userbot_teletips.update_profile(bio = f"{Emojis_teletips} {Quotes_teletips}" , last_name = f"| ⏰ {Time_teletips} | 📅 {Date_teletips}")
-                await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
+                Date_teletips = TimeZone_teletips.strftime("%b %d")
+                await Date_Time_Userbot_teletips.set_profile_photo(pp)
                 me = await Date_Time_Userbot_teletips.get_me()
                 photos = await Date_Time_Userbot_teletips.get_profile_photos("me")
                 try:
